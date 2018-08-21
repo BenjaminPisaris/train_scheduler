a0c70262017fe21b4fbd6dc2f2c0bd4686bd8783
Ben Pisaris <datcrisis@gmail.com>
2018-08-21 14:34:17 -0400

    added ability to upload trains to schedule

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,5 @@
+
+//Initialize Firebase
 var config = {
     apiKey: "AIzaSyC2bpLSa7JEitaTKwStG1ARPZt1WluP4ts",
     authDomain: "train-schedule-a87ee.firebaseapp.com",
@@ -7,8 +9,31 @@ var config = {
     messagingSenderId: "320241316446"
   };
   firebase.initializeApp(config);
-
   var data = firebase.database();
+
+  //Pull the data from Firebase to add to the table
   data.ref().on("value", function(snapshot) {
-      console.log(snapshot.val());
+    //Log data received from Firebase
+    console.log(snapshot.val());
+    //append table rows with data to table
+  });
+
+  //push completed form to firebase
+  //store all inputs to a variable
+  $("#add").on("click", function() {
+    var inputName = $("#name").val();
+    var inputDest = $("#destination").val();
+    var inputTime = $("#time").val();
+    var inputFreq = $("#frequency").val();
+    //store the input variables into a new object
+    var uploadMe = {
+        destination: inputDest,
+        firstTrain: inputTime,
+        frequency: inputFreq,
+        name: inputName
+    }
+    console.log(uploadMe);
+    //upload the object to FireBase
+    data.ref().push(uploadMe);
+
   });
